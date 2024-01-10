57650535c4d0869f3ac6789c2408de6c42724c1e
Selvarasha Thamilkkumaran <balapriyanka70@gmail.com>
2024-01-11 00:43:55 +0530

    modify code

--- WorldAPI/Controllers/CountryController.cs
+++ WorldAPI/Controllers/CountryController.cs
@@ -17,29 +17,70 @@ namespace WorldAPI.Controllers
         }
 
         [HttpGet]  //get all database datas
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult<IEnumerable<Country>> GetAll()
         {
-            return _dbContext.Countries.ToList();
+            var countries = _dbContext.Countries.ToList();
+            if (countries == null)
+            {
+                return NoContent();
+            }
+            return Ok(countries);
         }
 
         [HttpGet("{id:int}")] //get singel data from database with Id
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public ActionResult<Country> GetById(int id)
         {
-            return _dbContext.Countries.Find(id);
+            var countries = _dbContext.Countries.Find(id);
+            if (countries == null)
+            {
+                return NoContent();
+            }
+            return Ok(countries);
         }
 
-        [HttpPost] //post data from database 
+        [HttpPost] //post data from database
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         public ActionResult<Country> Create([FromBody]Country country)
         {
+            var result = _dbContext.Countries.AsQueryable().Where(x => x.Name.ToLower().Trim() == country.Name.ToLower().Trim()).Any(); // Name Check in database
+            if (result)
+            {
+                return Conflict("Country already exsitx in database");
+            }
             _dbContext.Countries.Add(country);
             _dbContext.SaveChanges();
-            return Ok();
+            return CreatedAtAction("GetById", new { id = country.Id }, country);
         }
 
-        [HttpPut] //data base data update with id
-        public ActionResult<Country> Update([FromBody]Country country)
+        [HttpPut] //data base data update with id 
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Country> Update(int id,[FromBody]Country country)
         {
-            _dbContext.Countries.Update(country);
+
+            if (country == null || id != country.Id)
+            {
+                return BadRequest();
+            }
+
+            var countryFromDb = _dbContext.Countries.Find(id);
+
+            if (countryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            countryFromDb.Name = country.Name;
+            countryFromDb.ShortName = country.ShortName;
+            countryFromDb.CountryCode = country.CountryCode;
+
+            _dbContext.Countries.Update(countryFromDb);
             _dbContext.SaveChanges();
             return Ok();
         }
