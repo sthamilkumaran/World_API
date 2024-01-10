d6208d289366ff9887726fb47bde02ac3e7051e4
Selvarasha Thamilkkumaran <balapriyanka70@gmail.com>
2024-01-11 00:06:19 +0530

    post update delete

--- WorldAPI/Controllers/CountryController.cs
+++ WorldAPI/Controllers/CountryController.cs
@@ -16,10 +16,39 @@ namespace WorldAPI.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpPost]
+        [HttpGet]  //get all database datas
+        public ActionResult<IEnumerable<Country>> GetAll()
+        {
+            return _dbContext.Countries.ToList();
+        }
+
+        [HttpGet("{id:int}")] //get singel data from database with Id
+        public ActionResult<Country> GetById(int id)
+        {
+            return _dbContext.Countries.Find(id);
+        }
+
+        [HttpPost] //post data from database 
         public ActionResult<Country> Create([FromBody]Country country)
         {
             _dbContext.Countries.Add(country);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut] //data base data update with id
+        public ActionResult<Country> Update([FromBody]Country country)
+        {
+            _dbContext.Countries.Update(country);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")] //database data delete with id
+        public ActionResult DeleteById(int id)
+        {
+            var country = _dbContext.Countries.Find(id);
+            _dbContext.Countries.Remove(country);
             _dbContext.SaveChanges();
             return Ok();
         }
