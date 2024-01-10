27fdc9a2bf0d46267c190a76d75da8ad9974e9bf
Selvarasha Thamilkkumaran <balapriyanka70@gmail.com>
2024-01-10 16:17:19 +0530

    Add project files.

---
+++ WorldAPI/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace WorldAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
---
+++ WorldAPI/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+#region Configure CORS
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("CustomPolicy", x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+#endregion
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("CustomPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
