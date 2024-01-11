58406646924a193213d63eab6351bae6f5124a21
Selvarasha Thamilkkumaran <balapriyanka70@gmail.com>
2024-01-11 22:42:34 +0530

    state database create

---
+++ WorldAPI/Models/States.cs
@@ -0,0 +1,14 @@
+﻿namespace WorldAPI.Models
+{
+    public class States
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Population { get; set; }
+
+        // country,State database relationship connection 
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+    }
+}
